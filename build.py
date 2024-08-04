f9970fc75de4f78a7c250cb29dc9be233cb99e4b
csae8092 <peter.andorfer@oeaw.ac.at>
2024-08-04 13:15:07 +0200

    fetch data script added

---
+++ build.py
@@ -0,0 +1,5 @@
+from scripts.fetch_data import fetch_data
+from scripts.build_static import build_static
+
+fetch_data()
+build_static()
---
+++ scripts/fetch_data.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+import requests
+
+GH_URL = "https://raw.githubusercontent.com/jerusalem-70-ad/jad-baserow-dump/main/data/"
+FILE_LIST = [
+    "passages.json",
+]
+DATA_DIR = os.path.join("html", "data")
+
+
+def fetch_data():
+
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    for x in FILE_LIST:
+        url = f"{GH_URL}{x}"
+        print(url)
+        data = requests.get(url).json()
+        save_path = os.path.join(DATA_DIR, x)
+        print(f"downloading {url} and saving it to {save_path}")
+        with open(save_path, "w", encoding="utf-8") as fp:
+            json.dump(data, fp, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    fetch_data()
