1a72d6ce026d1326ebbca3dfe0d939c9374f3c3c
csae8092 <peter.andorfer@oeaw.ac.at>
2024-08-13 13:11:07 +0200

    more columns in passages list view

--- html/js/passages.js
+++ html/js/passages.js
@@ -1,15 +1,27 @@
 const dataUrl = "data/passages.json"
 
+const colConf = [
+    { title: "ID", field: "jad_id", visible: false },
+    { title: "Quote", field: "passage", formatter: "textarea", headerFilter: "input", minWidth: 400 },
+    { title: "Work", field: "work", mutator:mutateM2mField, mutatorParams: {labelField: "name"}, headerFilter: "input" } ,
+    { title: "Bible", field: "biblical_references", mutator: mutateSelectField, headerFilter: "input" },
+    { title: "Language", field: "language", mutator: mutateSingleSelectField, headerFilter: "list", headerFilterParams: { valuesLookup: true, clearable: true } },
+]
+
 var table = new Tabulator("#tabulator-table", {
-    ajaxURL: dataUrl, 
-    ajaxResponse:function(url, params, response){
-        //url - the URL of the request
-        //params - the parameters passed with the request
-        //response - the JSON object returned in the body of the response.
+    ajaxURL: dataUrl,
+    ajaxResponse: function (url, params, response) {
         let row_data = [...Object.values(response)];
         return row_data;
     },
-    autoColumns:true,
-    height:400,
-    layout:"fitColumns"
+    height: 600,
+    layout: "fitColumns",
+    responsiveLayout:"collapse",
+    columns: colConf
+});
+
+table.on("rowClick", function(e, row){
+    var data = row.getData();
+    var url = `${data["jad_id"]}.html`
+    window.open(url);
 });
